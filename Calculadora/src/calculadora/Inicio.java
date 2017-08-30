e8cda07f12a4ab81ff7a80e9782a05dc399feae7
zicme <skate.pipe21@gmail.com>
2017-08-30 20:44:21 -0300

    pipe
    Reducir codigo

--- Calculadora/src/calculadora/Inicio.java
+++ Calculadora/src/calculadora/Inicio.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package calculadora;
 
 import java.util.Scanner;
@@ -12,18 +7,41 @@ import java.util.Scanner;
  * @author Alumno
  */
 public class Inicio {
+
     public float r;
     public float areaCirculo;
     public float perimetroCirculo;
-   
-    public Inicio(float r,float areaCirculo,float perimetroCirculo) {
+    public float Base;
+    public float Altura;
+    public float AreaTriangulo;
+    public float PerimetroTriangulo;
+    public float Lado;
+    public float AreaCuadrado;
+    public float PerimetroCuadrado;
+    public float AreaRectangulo;
+    public float PerimetroRectangulo;
+    public float LadoA;
+    public float LadoB;
+
+    public Inicio(float AreaCuadrado, float AreaRectangulo, float PerimetroCuadrado, float PerimetroRectangulo, float LadoA, float LadoB, float Lado, float PerimetroTriangulo, float AreaTriangulo, float Altura, float r, float areaCirculo, float perimetroCirculo, float Base) {
         this.r = r;
-        this.areaCirculo=areaCirculo;
-        this.perimetroCirculo=perimetroCirculo;
+        this.Lado = Lado;
+        this.PerimetroTriangulo = PerimetroTriangulo;
+        this.Base = Base;
+        this.AreaTriangulo = AreaTriangulo;
+        this.Altura = Altura;
+        this.areaCirculo = areaCirculo;
+        this.perimetroCirculo = perimetroCirculo;
+        this.LadoA = LadoA;
+        this.LadoB = LadoB;
+        this.AreaRectangulo = AreaRectangulo;
+        this.AreaCuadrado = AreaCuadrado;
+        this.PerimetroRectangulo = PerimetroRectangulo;
+        this.PerimetroCuadrado = PerimetroCuadrado;
     }
+
     public void menu() {
-        int opcion;
-        int opcion2;
+        int opcion, opcion2, opcion3, opcion4, opcion5;
         Scanner sc = new Scanner(System.in);
         do {
             System.out.println("Bienvenido");
@@ -39,44 +57,133 @@ public class Inicio {
                     System.out.println("[1]-Area");
                     System.out.println("[2]-Perimetro");
                     opcion2 = sc.nextInt();
-                    menu2(opcion2);
+                    Circulo(opcion2);
                     break;
                 case 2:
+                    System.out.println("Triangulo");
+                    System.out.println("[1]-Area");
+                    System.out.println("[2]-Perimetro");
+                    opcion3 = sc.nextInt();
+                    Triangulo(opcion3);
                     break;
                 case 3:
+                    System.out.println("Cuadrado");
+                    System.out.println("[1]-Area");
+                    System.out.println("[2]-Perimetro");
+                    opcion4 = sc.nextInt();
+                    Cuadrado(opcion4);
                     break;
                 case 4:
+                    System.out.println("Rectangulo");
+                    System.out.println("[1]-Area");
+                    System.out.println("[2]-Perimetro");
+                    opcion5 = sc.nextInt();
+                    Rectangulo(opcion5);
                     break;
                 default:
             }
         } while (opcion != 0);
     }
 
-    public void menu2(int n) {
+    public void Circulo(int n) {
         float d;
-        Scanner ff=new Scanner(System.in);
-        switch(n){
+        Scanner ff = new Scanner(System.in);
+        switch (n) {
+
             case 1:
                 System.out.println("Ingresa radio");
-                r=ff.nextFloat();
-                areaCirculo=(float) (3.14*r*r);
-                System.out.println("El area del circulo es:"+areaCirculo);
+                r = ff.nextFloat();
+                areaCirculo = (float) (3.14 * r * r);
+                System.out.println("El area del circulo es:" + areaCirculo);
                 break;
             case 2:
                 System.out.println("Ingrese Radio");
-                r=ff.nextFloat();
-                d=r+r;
-                perimetroCirculo=(float) ((3.14)*(d));
-                System.out.println("El perimetro del circulo es:"+perimetroCirculo);
+                r = ff.nextFloat();
+                d = r + r;
+                perimetroCirculo = (float) ((3.14) * (d));
+                System.out.println("El perimetro del circulo es:" + perimetroCirculo);
                 break;
-            default: System.out.println("Error");
-            
+            default:
+                System.out.println("Error");
+
         }
-        
-    
-    
+
     }
-    
-    
-    
+
+    public void Triangulo(int n) {
+        Scanner ff = new Scanner(System.in);
+        switch (n) {
+            case 1:
+                System.out.println("Ingresa Base");
+                Base = ff.nextFloat();
+                System.out.println("Ingrese Altura");
+                Altura = ff.nextFloat();
+                AreaTriangulo = Base * Altura / 2;
+                System.out.println("El area del triangulo es de:" + AreaTriangulo);
+                break;
+            case 2:
+                //triangulo equilatero tiene todos sus lados iguales 
+                System.out.println("Ingrese Lado del triangulo Equilátero:");
+                Lado = ff.nextFloat();
+                PerimetroTriangulo = Lado * 3;
+                System.out.println("El perimetro del triangulo es de:" + PerimetroTriangulo);
+                break;
+            default:
+                System.out.println("Error");
+
+        }
+
+    }
+
+    public void Cuadrado(int n) {
+        Scanner ff = new Scanner(System.in);
+        switch (n) {
+            case 1:
+                System.out.println("Ingresa Lado A");
+                LadoA = ff.nextFloat();
+                System.out.println("Ingrese Lado B");
+                LadoB = ff.nextFloat();
+                AreaCuadrado = LadoA * LadoB;
+                System.out.println("El area del Cuadrado es de:" + AreaCuadrado);
+                break;
+            case 2:
+                System.out.println("Ingrese Lado A:");
+                LadoA = ff.nextFloat();
+                System.out.println("Ingrese Lado B:");
+                LadoB = ff.nextFloat();
+                PerimetroCuadrado = (LadoA * 2) + (LadoB * 2);
+                System.out.println("El perimetro del Cuadrado es de:" + PerimetroCuadrado);
+                break;
+            default:
+                System.out.println("Error");
+
+        }
+
+    }
+
+    public void Rectangulo(int n) {
+        Scanner ff = new Scanner(System.in);
+        switch (n) {
+            case 1:
+                System.out.println("Ingresa Base");
+                Base = ff.nextFloat();
+                System.out.println("Ingrese Altura");
+                Altura = ff.nextFloat();
+                AreaRectangulo = Base * Altura;
+                System.out.println("El area del rectangulo es de:" + AreaRectangulo);
+                break;
+            case 2:
+                System.out.println("Ingresa Base");
+                Base = ff.nextFloat();
+                System.out.println("Ingrese Altura");
+                Altura = ff.nextFloat();
+                PerimetroRectangulo = (Base * 2) + (Altura * 2);
+                System.out.println("El perimetro del Rectangulo es de:" + PerimetroRectangulo);
+                break;
+            default:
+                System.out.println("Error");
+
+        }
+
+    }
 }
